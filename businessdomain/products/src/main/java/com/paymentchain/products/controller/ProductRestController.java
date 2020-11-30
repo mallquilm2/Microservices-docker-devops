e8671930ab2ada70a35a12191a01726bc55c1bec
miguel <miguel@MacBook-Pro.local>
2020-11-29 23:29:29 -0500

    centralizo configuracion de entornos y perfiles

--- businessdomain/products/src/main/java/com/paymentchain/products/controller/ProductRestController.java
+++ businessdomain/products/src/main/java/com/paymentchain/products/controller/ProductRestController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,6 +29,9 @@ public class ProductRestController {
     
     @Autowired
     ProductRepository customerRepository;
+    
+    @Value("${user.role}")
+    private String role;
           
      @GetMapping()
     public List<Product> list() {
